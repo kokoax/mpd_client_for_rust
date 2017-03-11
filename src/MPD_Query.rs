e0d09ea9152285e2ea86368d2297745f539f2cfb
kokoax <yomo4808@gmail.com>
2017-03-11 23:56:49 +0900

    3/11 last

--- src/MPD_Query.rs
+++ src/MPD_Query.rs
@@ -1,23 +1,24 @@
 pub mod MPD_Query {
     use std;
     use std::io::{Read, Write};
-    // use std::string::String;
-    // use std::io::prelude::*;
-    // use std::net::{TcpStream,SocketAddrV4,Ipv4Addr};
 
+    // get tcp connection(socket) to mpd
     pub fn get_mpd_socket(addr: std::net::Ipv4Addr, port: u16) -> std::net::TcpStream {
+        let mut mpd: std::net::TcpStream = std::net::TcpStream::connect(std::net::SocketAddrV4::new(addr, port))
+            .expect("Failed get TCP socket(MPD_Query::get_mpd_socket)");
+
         // TODO without timeout
         // So, specify end keyword such.
-        let mut mpd: std::net::TcpStream = std::net::TcpStream::connect(std::net::SocketAddrV4::new(addr, port)).unwrap();
         let _ = mpd.set_read_timeout(Some(std::time::Duration::new(0,1)));
 
-        // Receive message("OK MPD $mpd_version") from mpd when after connect mpd. Throw to dustbox.
+        // Receive message("OK MPD $mpd_version") from mpd when after connect mpd, that throw to dustbox.
         let mut buf: std::string::String = std::string::String::new();
         let _ = mpd.read_to_string(&mut buf);
 
         return mpd;
     }
 
+    // example. convert of "song: aaa\npath: /bbb/ccc\n" to {"song": "aaa", "path": "/bbb/ccc"}
     fn to_map(data: String) -> std::collections::HashMap<String, String> {
         let split_tmp: std::vec::Vec<&str> = data.split("\n").collect();
         let mut split_list: std::vec::Vec<Vec<&str>> = split_tmp
@@ -25,7 +26,7 @@ pub mod MPD_Query {
             .map(|x| x.split(": ").collect())
             .collect();
 
-        // mpd send "OK" receive's last. It throw to dust.
+        // mpd send "OK" receive's last, that throw to dust.
         let _ = split_list.pop();
         let _ = split_list.pop();
         let mut map: std::collections::HashMap<String, String> = std::collections::HashMap::new();
@@ -37,7 +38,27 @@ pub mod MPD_Query {
         return map.clone();
     }
 
-    // fn currentsong(mpd: &mut TcpStream) -> std::collections::HashMap<String, String> {
+    // fn ls_to_map(data: String) -> std::vec::Vec<std::collections::HashMap<String, String>> {
+    fn ls_to_map(data: String) -> std::collections::HashMap<String, String> {
+        let split_tmp: std::vec::Vec<&str> = data.split("\n").collect();
+        let mut split_list: std::vec::Vec<Vec<&str>> = split_tmp
+            .into_iter()
+            .map(|x| x.split(": ").collect())
+            .collect();
+
+        // mpd send "OK" receive's last, that throw to dust.
+        let _ = split_list.pop();
+        let _ = split_list.pop();
+        let mut map: std::collections::HashMap<String, String> = std::collections::HashMap::new();
+
+        for i in 0..split_list.len() {
+            map.insert(split_list[i][0].to_string(), split_list[i][1].to_string());
+        }
+
+        return map.clone();
+    }
+
+    // get currentsong infomation
     pub fn currentsong(mpd: &mut std::net::TcpStream) -> std::collections::HashMap<String, String> {
         let mut buf: std::string::String = std::string::String::new();
 
@@ -56,20 +77,22 @@ pub mod MPD_Query {
     // pub fn ls_playlist(mpd: &mut std::net::TcpStream, path: &'static str) -> Vec<String> {
     // }
     // pub fn ls(mpd: &mut std::net::TcpStream, path: &'static str) -> std::vec::Vec<String> {
-    //     let mut buf: std::string::String = std::string::String::new();
-    //     let mut ls: std::vec::Vec<&str>;
+    pub fn ls(mpd: &mut std::net::TcpStream, path: &'static str) -> std::collections::HashMap<String, String> {
+        let mut buf: std::string::String = std::string::String::new();
 
-    //     let _ = mpd.write(format!("{} {}\n", "lsinfo", path).as_bytes());
-    //     let _ = mpd.read_to_string(&mut buf);
+        let _ = mpd.write(format!("{} {}\n", "lsinfo", path).as_bytes());
+        let _ = mpd.read_to_string(&mut buf);
 
-    //     let ls: std::vec::Vec<&str> = buf.split("\n").collect();
+        // return to_map(buf);
+        let ls: std::vec::Vec<&str> = buf.split("\n").collect();
 
-
-    //     return ls.into_iter()
-    //         .map(|x| {
-    //              let tmp: std::vec::Vec<&str> = x.split(": ").collect();
-    //              return tmp[1].to_string();
-    //         }).collect();
-    // }
+        let ret = ls.into_iter().map(|x| {
+                 let tmp: std::vec::Vec<&str> = x.split(": ").collect();
+                 let ret: std::string::String = tmp[1].to_string();
+                 println!("{}", ret);
+                 return ret;
+            }).collect();
+        return ret
+    }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 mod MPD_Query;
-use std::net::{TcpStream,SocketAddrV4,Ipv4Addr};
+// use std::net::{TcpStream,SocketAddrV4,Ipv4Addr};
 
 fn main() {
-    let mut mpd = MPD_Query::MPD_Query::get_mpd_socket(Ipv4Addr::new(127,0,0,1), 6600);
+    let mut mpd = MPD_Query::MPD_Query::get_mpd_socket(std::net::Ipv4Addr::new(127,0,0,1), 6600);
+
+    let ls = MPD_Query::MPD_Query::ls(&mut mpd, "/");
     let current = MPD_Query::MPD_Query::currentsong(&mut mpd);
+
+    println!("{}", ls.len());
+    for i in 0..ls.len() {
+        println!("{}", ls[i]);
+    }
+
     for key in current.keys() {
         println!("{}: {}", key, current.get(key).unwrap());
     }
